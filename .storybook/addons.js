18d60a419e2941ae3100dceb62b55d611ccfbf8a
jGissinger <julien.gissinger@prestashop.com>
2020-05-14 17:31:30 +0200

    Implement storybook docspage

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,2 @@
 import '@storybook/addon-actions/register';
-import '@storybook/addon-docs/register';
 import '@storybook/addon-knobs/register';
---
+++ .storybook/presets.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    name: '@storybook/addon-docs/vue/preset',
+    options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+    },
+  },
+];
