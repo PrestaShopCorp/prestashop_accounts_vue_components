b2fb97b245f0ee12f9160a190134868ca91626a5
jGissinger <julien.gissinger@prestashop.com>
2020-05-26 10:57:19 +0200

    Import bootstrap icons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,12 @@
 import { configure } from '@storybook/vue';
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
+
 // import css style
 import 'bootstrap-vue/dist/bootstrap-vue';
 import 'prestakit/dist/css/bootstrap-prestashop-ui-kit.css';
 
 Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 configure(require.context('../src', true, /\.stories\.(jsx?|mdx)$/), module);
