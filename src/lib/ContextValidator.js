78534585e40692b2c2a60e16ddf20f4c123f99c4
Xavier Gouley <xavier.gouley@gmail.com>
2020-06-15 10:34:26 +0200

    optional values with Julian, to be finetuned by Julian

--- src/lib/ContextValidator.js
+++ src/lib/ContextValidator.js
@@ -2,19 +2,19 @@ import Joi from '@hapi/joi';
 
 export const shopSchema = Joi.object().keys({
     id: Joi.number().integer().positive().required(),
-    name: Joi.string().required().allow(''), // TODO required ? can be '' ?
+    name: Joi.string().required().min(1).max(128),
     url: Joi.string().uri().required(),
 });
 
 export const shopGroupSchema = Joi.object().keys({
     id: Joi.number().integer().positive().required(),
-    name: Joi.string().required().allow(''), // TODO required ? can be '' ?
+    name: Joi.string().required().min(1).max(128),
     shops: Joi.array().items(shopSchema).min(1).max(128).required(),
 });
 
 export const userSchema = Joi.object().keys({
-    email: Joi.string().email({ tlds: false }).allow(''), // TODO : valeur si pas la ?
-    emailIsValidated: Joi.boolean().required(), // TODO : tjrs là ?
+    email: Joi.string().email({ tlds: false }).allow(null).default(null),
+    emailIsValidated: Joi.boolean().default(false),
 });
 
 export const contextSchema = Joi.object().keys({
@@ -22,6 +22,6 @@ export const contextSchema = Joi.object().keys({
     psAccountIsEnabled: Joi.boolean().required(),
     currentShop: shopSchema.optional().allow(null).default(null),
     shops: Joi.array().items(shopGroupSchema).required().min(1).max(128),
-    user: userSchema.required(), // TODO : toujours là ?
+    user: userSchema.optional().allow({}).default({ email: null, emailIsValidated: false }),
     onboardingLink: Joi.string().uri().optional().allow(null).allow('').default(null),
 });
