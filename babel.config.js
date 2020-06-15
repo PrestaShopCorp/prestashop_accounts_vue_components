21776b2143d23a31c251e4218af36eb2160694e2
Xavier Gouley <xavier.gouley@gmail.com>
2020-06-15 16:38:53 +0200

    MDX in da place

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    ['@vue/cli-plugin-babel/preset', {
+      jsx: false // To fix a MDX bug 'h is not defined' : https://github.com/storybookjs/storybook/issues/11094
+    }]
   ],
 };
