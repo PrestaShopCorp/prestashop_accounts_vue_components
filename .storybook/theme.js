fae54f0f7d0f883e11681a1453d0e304261c06e7
Xavier Gouley <xavier.gouley@gmail.com>
2020-06-17 14:24:59 +0200

    theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,11 +3,13 @@ import { create } from '@storybook/theming/create';
 export default create({
     base: 'light',
 
-    colorPrimary: '#FF0076',
-    colorSecondary: '#301F91',
-    appBg: '#E5E1F9',
+    colorPrimary: '#DF0067',
+    colorSecondary: '#251B5B',
+    appBg: '#312525',
     appBorderRadius: 5,
     fontBase: '"Open Sans", Helvetica, Verdana, sans-serif',
+
+    textColor: 'white',
 
     brandTitle: 'PS Accounts Vue Components',
     brandImage: 'https://www.shareicon.net/data/256x256/2015/10/06/112712_development_512x512.png',
