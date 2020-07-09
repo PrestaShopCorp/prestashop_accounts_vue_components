2f544d4f8ecbfbac7a23e2e9ce4e2b4be3bbf4b7
Xavier Gouley <xavier.gouley@gmail.com>
2020-07-09 10:45:04 +0200

    i18n

--- src/full.js
+++ src/full.js
@@ -5,8 +5,21 @@ import AccountNotInstalled from '@/components/alert/AccountNotInstalled';
 import AccountNotEnabled from '@/components/alert/AccountNotEnabled';
 import MultiStoreSelector from '@/components/alert/MultiStoreSelector';
 import {isOnboardingCompleted} from '@/lib/Helpers';
+import locale from '@/locale';
+
+const install = function(Vue, opts = {}) {
+  console.log('### install called with ', !!Vue, opts);
+  locale.use(opts.locale);
+  locale.i18n(opts.i18n);
+  Object.keys(Components).forEach((name) => {
+    Vue.component(name, Components[name]);
+  });
+};
 
 const Components = {
+  locale: locale.use,
+  i18n: locale.i18n,
+  install,
   PsAccounts,
   Account,
   AccountNotInstalled,
@@ -14,9 +27,9 @@ const Components = {
   MultiStoreSelector,
 };
 
-Object.keys(Components).forEach((name) => {
-  Vue.component(name, Components[name]);
-});
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
 
 export default Components;
 
