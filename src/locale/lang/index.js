22656860f9884c2a217210e33101edc35ead8981
atourneriePresta <anthony.tournerie@prestashop.com>
2021-01-14 17:38:29 +0100

    fix/ load locale languages or en if needed + refactor switch

---
+++ src/locale/lang/index.js
@@ -0,0 +1,19 @@
+import de from '@/locale/lang/de.json';
+import en from '@/locale/lang/en.json';
+import es from '@/locale/lang/es.json';
+import fr from '@/locale/lang/fr.json';
+import it from '@/locale/lang/it.json';
+import nl from '@/locale/lang/nl.json';
+import pl from '@/locale/lang/pl.json';
+import pt from '@/locale/lang/pt.json';
+
+export default {
+  de,
+  en,
+  es,
+  fr,
+  it,
+  nl,
+  pl,
+  pt,
+}
