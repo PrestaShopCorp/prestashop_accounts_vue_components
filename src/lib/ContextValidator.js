462b2881ae9f0d2d3a374a1308845e178a930cc6
André <andre.amorim@prestashop.com>
2021-04-13 14:16:55 +0200

    fix: changed context validation property names & types to the new ones

--- src/lib/ContextValidator.js
+++ src/lib/ContextValidator.js
@@ -1,15 +1,15 @@
 import Joi from '@hapi/joi';
 
 export const shopSchema = Joi.object().keys({
-  id: Joi.number().integer().positive().required(),
+  shopId: Joi.string().required(),
   name: Joi.string().required().min(1).max(128),
   url: Joi.string().uri().required(),
   domain: Joi.string().pattern(/([a-z0-9]+(-[a-z0-9]+)*)+/i, 'domain').required(),
-  domainSsl: Joi.string().pattern(/([a-z0-9]+(-[a-z0-9]+)*)+/i, 'domainSsl').required(),
+  sslDomain: Joi.string().pattern(/([a-z0-9]+(-[a-z0-9]+)*)+/i, 'sslDomain').required(),
 }).unknown(true);
 
 export const shopGroupSchema = Joi.object().keys({
-  id: Joi.number().integer().positive().required(),
+  shopId: Joi.string().required(),
   name: Joi.string().required().min(1).max(128),
   shops: Joi.array().items(shopSchema).min(1).max(128)
     .required(),
