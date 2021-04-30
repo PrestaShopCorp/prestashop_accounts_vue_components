189ee9e8fc0b1e49cb53bdd2cbf111480730c0d7
atourneriePresta <anthony.tournerie@prestashop.com>
2021-04-30 23:49:44 +0200

    fix: single quote to build

--- src/components/crossdomains/linkShopCrossDomain.js
+++ src/components/crossdomains/linkShopCrossDomain.js
@@ -2,7 +2,7 @@ import * as zoid from 'zoid/dist/zoid.frameworks';
 
 export default zoid.create({
   tag: 'crossdomains-account-link-shop',
-  url: process.env.VUE_APP_ACCOUNTS_UI_URL || "https://accounts.prestashop.localhost/en/?cdc=true",
+  url: process.env.VUE_APP_ACCOUNTS_UI_URL || 'https://accounts.prestashop.localhost/en/?cdc=true',
   context: 'iframe',
 
   // The size of the component on their page. Only px and % strings are supported
