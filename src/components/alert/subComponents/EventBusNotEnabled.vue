5e39a5a2b145d613983029dcb6e16a65c6b0910c
Thomas N <Quetzacoalt91@users.noreply.github.com>
2021-11-26 08:32:36 +0000

    Handle case where PS EventBus is not enabled (#119)
    * Handle case where PS EventBus is not enabled
    
    * feat: improve ps_eventbus state tracking
    
    Co-authored-by: Antoine Metifeu <ametifeu@gmail.com>

---
+++ src/components/alert/subComponents/EventBusNotEnabled.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-alert
+    variant="warning"
+    show
+  >
+    <h3>{{ t('psaccounts.alertEventBusNotEnabled.title') }}</h3>
+    <p>
+      {{ t('psaccounts.alertEventBusNotEnabled.message') }}.
+    </p>
+    <p class="mt-2">
+      <b-button
+        v-if="!isLoading"
+        variant="primary"
+        @click="enableEventBus()"
+      >
+        {{ t('psaccounts.alertEventBusNotEnabled.enableButton') }}
+      </b-button>
+      <b-link
+        href="#"
+        disabled
+        v-else
+      >
+        {{ t('psaccounts.alertEventBusNotEnabled.loading') }}
+      </b-link>
+    </p>
+  </b-alert>
+</template>
+
+<script>
+  import Alert from './Alert';
+  import enableModule from '../../../lib/moduleManager/EnableModule';
+
+  /**
+   * This sub-component can be used in a custom integration when the `EventBus`
+   * component does not meets special needs. This part will display a warning message
+   * telling the PS EventBus module is not enabled on the shop (and a button to enable it).
+   */
+  export default {
+    name: 'AlertEventBusNotEnabled',
+    mixins: [Alert],
+    methods: {
+      enableEventBus() {
+        this.isLoading = true;
+
+        this.$tracking.track('[ACC] PSEventBus Enable Button Clicked');
+
+        enableModule(
+          'ps_eventbus',
+          this.link,
+          this.psIs17,
+        ).catch(() => {
+          this.isLoading = false;
+          this.$emit('hasError');
+        });
+      },
+    },
+  };
+</script>
