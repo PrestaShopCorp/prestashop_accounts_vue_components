24271db1c07c7d60d7b05a2bc5bad5fe2e17e7b2
atourneriePresta <anthony.tournerie@prestashop.com>
2022-01-06 17:34:48 +0100

    feat: front url optionnal after string + allow null

--- src/lib/ContextValidator.js
+++ src/lib/ContextValidator.js
@@ -20,7 +20,7 @@ export const shopSchema = Joi.object().keys({
   domain: Joi.string().pattern(/([a-z0-9]+(-[a-z0-9]+)*)+/i, 'domain').allow(null).required(),
   domainSsl: Joi.string().pattern(/([a-z0-9]+(-[a-z0-9]+)*)+/i, 'domainSsl').allow(null).required(),
   employeeId: Joi.number().allow(null),
-  frontUrl: Joi.optional().string().uri(),
+  frontUrl: Joi.string().optional().uri().allow(null),
   id: Joi.string().required(),
   moduleName: Joi.string(),
   multishop: Joi.boolean(),
