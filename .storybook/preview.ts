8670484924bd0a7c2cd6274b57487b57a6fa2c2a
Antoine Metifeu <ametifeu@gmail.com>
2022-02-01 18:04:09 +0100

    import prestakit in storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import { Segment, VueCompositionAPI } from "@/ExternalDependencies";
 
+import "prestakit/scss/application.scss";
+
 Vue.use(Segment, {
   id: process.env.VUE_APP_SEGMENT,
   pageCategory: "ps_accounts-ui",
