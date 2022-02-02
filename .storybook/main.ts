3d4d1c44787da60822b974003e5588c70b6bd1ce
Antoine Metifeu <ametifeu@gmail.com>
2022-02-02 17:44:22 +0100

    fix: svg & typescript support for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ module.exports = {
   stories: ["../src/**/*.stories.@(js|mdx)"],
   logLevel: "debug",
   addons: [
+    "@storybook/preset-typescript",
     "@storybook/addon-actions",
     "@storybook/addon-controls",
     {
@@ -22,10 +23,19 @@ module.exports = {
       "@": path.resolve(__dirname, "../src"),
     };
 
+    let rule = config.module.rules.find(
+      (r) =>
+        r.test &&
+        r.test.toString().includes("svg") &&
+        r.loader &&
+        r.loader.includes("file-loader")
+    );
+    rule.test =
+      /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+
     config.module.rules.push({
-      test: /\.ts$/,
-      loader: "ts-loader",
-      options: { appendTsSuffixTo: [/\.vue$/] },
+      test: /\.svg$/,
+      use: ["vue-svg-loader"],
     });
 
     config.module.rules.push({
