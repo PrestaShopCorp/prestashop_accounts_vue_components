9326f7c6ee8921d6f9982927a3d412aadf8496a0
Antoine Metifeu <ametifeu@gmail.com>
2022-02-03 15:49:24 +0100

    fix: require.context in jest

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['@vue/cli-plugin-babel/preset'];
+  const plugins = [];
+
+  if (process.env.NODE_ENV === 'test') {
+    plugins.push('transform-require-context');
+  }
+
+  return {
+    presets,
+    plugins,
+  };
 };
