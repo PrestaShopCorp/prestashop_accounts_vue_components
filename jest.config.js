f8efaeb4abe14714dd2e178291fbf905f17d708e
Antoine Metifeu <ametifeu@gmail.com>
2022-02-04 16:21:46 +0100

    test: add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.(js|ts|vue)'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/main.ts',
+  ],
 };
