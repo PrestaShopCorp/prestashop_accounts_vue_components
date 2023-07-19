d80e7e1d881d39efae990b9ca776f4ff45e8679e
Thomas Nabord <thomas.nabord@prestashop.com>
2023-07-19 18:43:54 +0200

    Prefix all CSS rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-prefix-selector': {
+      prefix: '#psaccounts',
+      transform: function(prefix, selector, prefixedSelector) {
+        if (selector.match(/^(html|body)/)) {
+          return selector.replace(/^([^\s]*)/, `$1 ${prefix}`);
+        } else if (selector.match(/^(\#psaccounts)/)) {
+          return selector;
+        } else {
+          return prefixedSelector;
+        }
+      }
+    },
   }
 };
